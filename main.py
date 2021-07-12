30b0bcfa77968b476fb23f6f65283f4e4026d78a
Eliza <kasperek.eliza@gmail.com>
2021-07-12 13:55:46 +0200

    Nowe ćwiczenie z widżetów

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+class WidgetsExample(GridLayout):
+    count = 1
+    my_text = StringProperty("1")
+    def on_button_click(self):
+        print("Kliknięty klawisz")
+        # self.my_text = "Nacisnąłeś mnie"
+        self.count += 1
+        self.my_text = str(self.count)
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
